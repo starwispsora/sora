23ab917ea93ce8c3e5d4ce8a282e16e147b81d55
starwispsora <142126413+starwispsora@users.noreply.github.com>
2024-07-25 21:19:55 +0900

    Create WhiteFiringHeart.py

---
+++ WhiteFiringHeart.py
@@ -0,0 +1,12 @@
+def print_heart():
+    print("you make my circuit heart brighter. heart.")
+    
+    for row in range(6):
+        for col in range(7):
+            if (row == 0 and col % 3 != 0) or (row == 1 and col % 3 == 0) or (row - col == 2) or (row + col == 8):
+                print("❤", end='')
+            else:
+                print(" ", end='')
+        print()
+
+print_heart()
